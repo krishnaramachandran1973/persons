b23e17c892566b1bb1bbb30851b304e3883d9df3
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-04-27 19:04:07 +0530

    Removed unnecessary imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Login from "./Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import RoutesSetup from "./RoutesSetup";
